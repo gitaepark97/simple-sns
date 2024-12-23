d1afb490f8a98059562e1bba725e9b0dd9cc4e90
gitaepark97 <parkkitae7@gmail.com>
2024-12-23 10:33:06 +0900

    기본 프로젝트 설정

---
+++ src/test/java/hugo/simplesns/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package hugo.simplesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
